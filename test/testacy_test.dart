2cb5f98941370b34e9e5a9fdabf0f65119c268c6
Joe Conway <joe.conway@stablekernel.com>
2016-05-05 12:36:51 -0400

    added travis

---
+++ test/testacy_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:testacy/testacy.dart';
+import 'package:test/test.dart';
+import 'dart:async';
+
+void main() {
+  test("Test things", () async {
+    print("1");
+    await new Future.delayed(new Duration(seconds: 2));
+    print("2");
+    expect(true, true);
+    await new Future.delayed(new Duration(seconds: 2));
+    print("3");
+    expect(true, true);
+  });
+}
