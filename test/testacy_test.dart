56aa9170056a301a3097649f8cdd50e20dda5a6c
Joe Conway <joe.conway@stablekernel.com>
2016-05-05 12:38:15 -0400

    added travis

--- test/testacy_test.dart
+++ test/testacy_test.dart
@@ -6,12 +6,16 @@ import 'package:test/test.dart';
 import 'dart:async';
 
 void main() {
+  test("Not async", () {
+    expect(true, true);
+  });
+
   test("Test things", () async {
     print("1");
-    await new Future.delayed(new Duration(seconds: 2));
+    await new Future.delayed(new Duration(seconds: 5));
     print("2");
     expect(true, true);
-    await new Future.delayed(new Duration(seconds: 2));
+    await new Future.delayed(new Duration(seconds: 5));
     print("3");
     expect(true, true);
   });
